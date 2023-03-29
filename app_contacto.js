52f9fa46f9a39eb1470776a36c004b643ce6f472
Mario Garcia <mgarciac444@gmail.com>
2023-03-29 21:35:28 +0200

    Validaciones Hechas menos login

--- app_contacto.js
+++ app_contacto.js
@@ -1,35 +1,55 @@
-let formularios = document.getElementById('contacto');
-function validarNombre(){
-    let valor = formularios[0]['elements']['nombre']['value'];
-    let expresion = /^\b$/;//set un numero de palabras
+function validarNombre() {
+    let valor = document.getElementById("nombre");
+    let expresion = /^\w{3,15}$/;//de 3 a 15 letras
+    let span = document.getElementById('spanNombre');
 
-    if(expresion.test(valor)){
-        console.log('nombre correcto');
-    }else{
-        console.log('no funciona');
-    }
-}
-function validarApellidos(){
-    let valor = formularios[0]['elements']['apellidos']['value'];
-    let expresion = /^\b{2,20}\s\b{2,20}$/;//hecho con 2 apellidos
-
-    if(expresion.test(valor)){
-        console.log('apellido correcto');
-    }else{
-        console.log('no funciona');
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
     }
 }
 
-function validarCorreo(){
-    let valor = formularios[0]['elements']['correo']['value'];
-    let expresion = /^\w{5,25}\@gmail.com$/; //expr hecha
+function validarApellidos() {
+    let valor = document.getElementById("apellidos");
+    let expresion = /^\D{2,20}\s\D{2,20}$/;//hecho con 2 apellidos
+    let span = document.getElementById('spanApellido');
 
-    if(expresion.test(valor)){
-        console.log('correo correcto');
-    }else{
-        console.log('no funciona');
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
     }
 }
-function validarTextoAEnviar(){
-    //no sé si esto requiere validar una canción
+
+function validarCorreo() {
+    let valor = document.getElementById("correo");
+    let expresion = /^\w{5,25}\@gmail.com$/;
+    let span = document.getElementById('spanCorreo');
+
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }
+}
+
+function validarTexto() {
+    let valor = document.getElementById("texto");
+    let expresion = /(\w)/; //Que escriba algo con sentido
+    let span = document.getElementById('spanTexto');
+
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }
 }
--- app_registro.js
+++ app_registro.js
@@ -1,44 +1,98 @@
-let formularios = document.getElementById('registro');
-function validarNombre(){
-    let valor = formularios[0]['elements']['nombre']['value'];
+function validarNombre() {
+    let valor = document.getElementById("nombre");
+    let expresion = /^\w{3,15}$/;//de 3 a 15 letras
+    let span = document.getElementById('spanNombre');
 
-    let expresion = /^\b$/;//set un numero de palabras
-
-    if(expresion.test(valor)){
-        console.log('todo correcto');
-    }else{
-        console.log('no funciona');
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
     }
 }
-function validarApellidos(){
-    let valor = formularios[0]['elements']['apellidos']['value'];
-    let expresion = /^\b{2,20}\s\b{2,20}$/;//hecho con 2 apellidos
 
+function validarApellidos() {
+    let valor = document.getElementById("apellidos");
+    let expresion = /^\D{2,20}\s\D{2,20}$/;//hecho con 2 apellidos
+    let span = document.getElementById('spanApellido');
+
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }
 }
-function validarDNi(){
-    let valor = formularios[0]['elements']['dni']['value'];
-    let expresion = /^\d{8}\b$/;//expr hecha
+function validarDNI(){
+    let valor = document.getElementById("dni");
+    let expresion = /^\d{8}\D$/;//expr hecha
+    let span = document.getElementById('spanDNI');
+
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }    
 }
 function validarTel(){
-    let valor = formularios[0]['elements']['telefono']['value'];
+    let valor = document.getElementById("tlfn");
     let expresion = /^\d{9}$/; //expr: hecha
+    let span = document.getElementById('spanTlfn');
 
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }
 }
 function validarDir(){
-    let valor = formularios[0]['elements']['direccion']['value'];
-    let expresion = /^\b{5,20}\s\b{5,20}$/; 
+    let valor = document.getElementById("dir");
+    let expresion = /(?=.*[a-z])\w{5,35}/;
+    let span = document.getElementById('spanDir');
+
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }}
+
+function validarCorreo() {
+    let valor = document.getElementById("correo");
+    let expresion = /^\w{5,25}\@gmail.com$/;
+    let span = document.getElementById('spanCorreo');
+
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }
 }
-function validarCorreo(){
-    let valor = formularios[0]['elements']['correo']['value'];
-    let expresion = /^\w{5,25}\@gmail.com$/; //expr hecha
+
+function validarContrasenia() {
+    let valor = document.getElementById("contrasenia");
+    let span = document.getElementById('spanContrasenia');
+    let expresion = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d{2})(?=.*[-_.+@])([A-Za-z\d$@-_.%*]){8,15}/;
+
+    if (expresion.test(valor.value)) {
+        span.textContent = "Correcto";
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }
 }
-function validarContras(){
-    let valor = formularios[0]['elements']['contraseña']['value'];
-    let expresion = /^\w{5,25}$/; //expr hecha
-}
-function validarCondiciones(){
-    //aquí hay que ver como se valida
-}
+
+
 /******************EXPRESIONES REGULARES***************
  *  A la hora de evaluar el contenido de un elemento del formulario podemos realizar búsquedas por un patrón
  * 
