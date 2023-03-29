ec2aea5bdbb2b63c370ba0d12f460f94d4a8b104
Mario Garcia <mgarciac444@gmail.com>
2023-03-29 21:39:25 +0200

    Login con Validacion

--- app_login.js
+++ app_login.js
@@ -1,48 +1,29 @@
-let formularios = document.getElementById('login');
-console.log(formularios);
-function validarCorreo(){
+function validarCorreo() {
+    let valor = document.getElementById("correo");
+    let expresion = /^\w{5,25}\@gmail.com$/;
+    let span = document.getElementById('spanCorreo');
 
-    let valor = formularios[0]['elements']['correo']['value'];
-    let expresion = /^\w{5,25}\@gmail.com$/; //expr hecha
-    let span = document.getElementById('spancorreo');
-
-    if(expresion.test(valor)){
+    if (expresion.test(valor.value)) {
         span.textContent = "Correcto";
-        span.classList.add('valido');
-        span.classList.remove("invalido");
-        console.log("valido");
-    
-       }else{
-            span.textContent = "Incorrecto";
-            span.classList.add('invalid')
-            span.classList.remove('valid');
-            console.log("invalido");
-       }
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }
 }
 
+function validarContrasenia() {
+    let valor = document.getElementById("contrasenia");
+    let span = document.getElementById('spanContrasenia');
+    let expresion = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d{2})(?=.*[-_.+@])([A-Za-z\d$@-_.%*]){8,15}/;
 
-//la real
-function validarContrasenia(){
-    let valor = formularios[0]['elements']['contrasenia']['value'];
-    let span = document.getElementById('spancontras');
-    // /^$/ inicio cadena ORDENADA
-    //  sin orden 
-    //Pa$$w0rd. 
-    let expresion = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d{2})(?=.*[-_.+@])([A-Za-z\d$@-_.%*]|[^ ]){8,15}/;
-    //$@$!%*?&
-
-    if(expresion.test(valor)){
+    if (expresion.test(valor.value)) {
         span.textContent = "Correcto";
-        span.classList.add('valido');
-        span.classList.remove("invalido");
-        console.log("valido");
-    
-       }else{
-            span.textContent = "Incorrecto";
-            span.classList.add('invalid')
-            span.classList.remove('valid');
-            console.log("invalido");
-       }
+        span.style.color = "green";
+    } else {
+        span.textContent = "Incorrecto";
+        span.style.color = "red";
+    }
 }
 /* Contraseña: una mayúscula, una minúscula, 2 digitos seguidos, un signo entre los siguientes -_.+ 
 longitud >10
