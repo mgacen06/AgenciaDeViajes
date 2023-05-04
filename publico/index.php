8c445d1445acc0b886b659295d319aff51aed4ed
Mario Garcia <mgarciac444@gmail.com>
2023-05-04 20:05:33 +0200

    Nav y footer con css Igual en todo

--- publico/index.php
+++ publico/index.php
@@ -1,8 +1,6 @@
 <?php
-
 require_once("../administracion/Database/Database.php");
 $resultado = Database::getAllAlojamientos();
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,8 +10,9 @@ $resultado = Database::getAllAlojamientos();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="author" content="Lucía Ortiz y Mario García">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/navYFooter.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="../PFC/fontawesome/css/all.css">
+    <link rel="stylesheet" href="/publico/fontawesome/css/all.css">
     <link rel="shortcut icon" href="img/icono.ico" type="" id="icono">
     <title>Viajes Lario</title>
 </head>
@@ -37,36 +36,20 @@ $resultado = Database::getAllAlojamientos();
             <?php
             foreach ($resultado as $fila) {
                 echo "<div class='card'>";
-                    echo "<img class='imagen' alt='Avatar' src=" . $fila['imagen'] . "></img>";
-                    echo "<div class='info'>";
-                        echo "<h4 class='titulo'>" . $fila['nombreViaje'] . "</h4>";
-                        echo "<small>" . $fila['tipoEstancia'] . "</small>";
-                        echo "<p id='lugar'>" . $fila['ubicacion'] . "</p>";
-                        echo "<div class='precio'>" . $fila['precioPorNoche'] . " €</div>";
-                    echo "</div>";
+                echo "<img class='imagen' alt='Avatar' src=" . $fila['imagen'] . "></img>";
+                echo "<div class='info'>";
+                echo "<h4 class='titulo'>" . $fila['nombreViaje'] . "</h4>";
+                echo "<small>" . $fila['tipoEstancia'] . "</small>";
+                echo "<p id='lugar'>" . $fila['ubicacion'] . "</p>";
+                echo "<div class='precio'>" . $fila['precioPorNoche'] . " €</div>";
+                echo "</div>";
                 echo "</div>";
             }
             ?>
         </div>
     </main>
-    <footer>
-        <div class="redesSociales">
-            <a class="redes" href="https://www.instagram.com/mgacen06/"><i id="botontw" class="fab fa-instagram"></i>Instagram</a>
-            <a class="redes" href="https://twitter.com"><i id="botontw" class="fab fa-twitter"></i>Twitter</a>
-        </div>
-        <div class="infoFooter">
-            <div id="aboutus">
-                <!-- Sobre nosotros -->
-                <h4>Nuestra historia:</h4>
-                <p>Es una empresa que nació en 2004, por Lucía Ortiz y Mario García, llegando a posicionarse como una de las empresas de viajes con más usuarios registrados, en la que más de 50.000 personas al año confían para planificar sus vacaciones.</p>
-                <p>Visita nuestras redes sociales para conocer más.</p>
-            </div>
-            <div></div>
-            <br>
-            <small>Copyright © 2022 Empresa de venta de alojamientos. Todos los derechos reservados. C/ Bernardino Obregón 25, 28012. Madrid, España</small>
-        </div>
-    </footer>
+    <footer id="elFooter"></footer>
 </body>
-<script src="navYFooter.js"></script>
 <script src="app.js"></script>
+<script src="navYFooter.js"></script>
 </html>
--- publico/navYFooter.js
+++ publico/navYFooter.js
@@ -1,3 +1,4 @@
+// NAV
 let elNav=document.getElementById("elNav");
 
 let ulNav=document.createElement("ul");
@@ -15,7 +16,6 @@ iInicio.setAttribute("class","fa fa-plane");
 aInicio.appendChild(iInicio);
 liInicio.appendChild(aInicio);
 ulNav.appendChild(liInicio);
-
 
 let liAbout=document.createElement("li");
 let aAbout=document.createElement("a");
@@ -57,3 +57,55 @@ liLogin.appendChild(aLogin);
 ulNav.appendChild(liLogin);
 
 elNav.appendChild(ulNav);
+
+
+// FOOTER
+let elFooter=document.getElementById("elFooter");
+
+let divRedes =document.createElement("div");
+divRedes.setAttribute("class", "redesSociales");
+
+let aredes1 =document.createElement("a");
+aredes1.setAttribute("class", "redes");
+aredes1.setAttribute("href", "https://www.instagram.com/mgacen06");
+aredes1.textContent="Instagram";
+let iredes1=document.createElement("i")
+iredes1.setAttribute("id","botontw");
+iredes1.setAttribute("class", "fab fa-instagram");
+
+let aredes2 =document.createElement("a");
+aredes2.setAttribute("class", "redes");
+aredes2.setAttribute("href", "https://twitter.com");
+aredes2.textContent="Twitter";
+let iredes2=document.createElement("i")
+iredes2.setAttribute("id","botontw");
+iredes2.setAttribute("class", "fab fa-twitter");
+
+aredes1.appendChild(iredes1);
+aredes2.appendChild(iredes2);
+divRedes.appendChild(aredes1);
+divRedes.appendChild(aredes2);
+elFooter.appendChild(divRedes);
+
+let divFooter =document.createElement("div");
+divFooter.setAttribute("class","infoFooter");
+let divAbout=document.createElement("div");
+divAbout.setAttribute("id","aboutus");
+let h4about=document.createElement("h4");
+h4about.textContent="Nuestra historia";
+let pabout1=document.createElement("p");
+pabout1.textContent="Es una empresa que nació en 2004, por Lucía Ortiz y Mario García, llegando a posicionarse como una de las empresas de viajes con más usuarios registrados, en la que más de 50.000 personas al año confían para planificar sus vacaciones.";
+let pabout2=document.createElement("p");
+pabout2.textContent="Visita nuestras redes sociales para conocer más.";
+let small=document.createElement("small");
+let fecha = new Date();
+let anno = fecha.getFullYear();
+small.textContent=`Copyright © ${anno} Empresa de venta de alojamientos. Todos los derechos reservados. C/ Bernardino Obregón 25, 28012. Madrid, España`;
+small.style.color="white";
+
+divAbout.appendChild(h4about);
+divAbout.appendChild(pabout1);
+divAbout.appendChild(pabout2);
+divFooter.appendChild(divAbout);
+divFooter.appendChild(small);
+elFooter.appendChild(divFooter);
