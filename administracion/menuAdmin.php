22ca0a0ef8b0c0c721ec1759c76b24b1f000939d
lucia <luciaone34@gmail.com>
2023-05-19 12:36:25 +0200

    menuAdmin + css

---
+++ administracion/menuAdmin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Menu Adminsitración</title>
+</head>
+<body>
+   <nav>
+    <a href="menuAdmin.php">Administración</a>  
+    <!-- opciones de nav -->
+    <ul>
+        <!-- clase para los iconos -->
+        <li><a class="menuA"  href="estancias/index.php">Estancias</a></li>
+        <li><a class="menuA" href="reservas/index.php">Reservas</a></li> 
+        <li><a class="menuA" href="usuarios/index.php">Usuarios</a></li> 
+    </ul>
+   </nav>
+    <main>
+        <p>Crea, modifica y elimina</p>
+    </main>
+  <!-- ver si aquí pongo una tabla predeterminada -->
+</body>
+
+</html>
